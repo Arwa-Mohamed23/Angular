328c0a9ed76e1a5110eb52a4c6e5a4777d7219ce
Arwa-Mohamed23 <arwamohmd.32@gmail.com>
2025-01-18 17:18:04 +0200

    add portfolio

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import {HeroSectionComponent} from './hero-section/hero-section.component';
+import {AboutMeComponent} from './about-me/about-me.component';
+import {SkillsComponent} from './skills/skills.component';
+import {ProjectComponent} from './project/project.component';
+import {FooterComponent} from './footer/footer.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [HeroSectionComponent, AboutMeComponent, SkillsComponent, ProjectComponent,FooterComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'portfolio';
+}
---
+++ src/app/project/project.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project',
+  imports: [],
+  templateUrl: './project.component.html',
+  styleUrl: './project.component.css'
+})
+export class ProjectComponent {
+  projects = [
+    { title: 'Project 1', description: 'Description 1', link: '#' },
+    { title: 'Project 2', description: 'Description 2', link: '#' },
+    { title: 'Project 3', description: 'Description 3', link: '#' },
+  ];
+}
